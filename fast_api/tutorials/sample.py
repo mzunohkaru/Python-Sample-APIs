8513695fbfcf72046dcc1fbf1b4bf3ef7fba3e94
mzunohkaru <winkaisyu@gmail.com>
2024-01-15 15:41:00 +0900

    added to exsample.py

--- fast_api/tutorials/sample.py
+++ fast_api/tutorials/sample.py
@@ -8,15 +8,16 @@ app = FastAPI()
 async def index():
     return {"Data": "hello"}
 
+
 # http://127.0.0.1:8000/countries/japan
 @app.get("/countries/{country_name}")
 async def country(country_name):
     return {"国名": country_name}
 
 
-
 # http://127.0.0.1:8000/search/World
 # {"ID":1,"タイトル":null,"内容":"World"}
+
 
 # http://127.0.0.1:8000/search/Hello?id=2&title=TTT
 # {"ID":2,"タイトル":"TTT","内容":"Hello"}
